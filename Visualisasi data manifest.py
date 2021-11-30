681d4d85f7ac0f885ebbb9caec572f0bcd2bfdb0
Ahmad Haziq <88761883+haziqsyarrif@users.noreply.github.com>
2021-11-30 16:14:50 +0700

    Update Visualisasi data manifest.py

--- Visualisasi data manifest.py
+++ Visualisasi data manifest.py
@@ -1,4 +1,5 @@
-#Visualisasi Data
+#Program Visualisasi Data manifest_1.csv
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -6,18 +7,21 @@ import numpy as np
 N = 3
 ind = np.arange(N)  
 
+#Visualisasi data untuk banyak tiket terjual per destinasi
 df = pd.read_csv("manifest_1.csv")
 df['Destinasi'].value_counts().plot(kind='bar')
 plt.ylabel("Tiket Terjual")
 plt.xlabel("Destinasi Tujuan")
 plt.show()
 
+#Visualisasi data untuk banyak tiket terjual per destinasi
 df['Waktu'].value_counts().plot(kind='bar')
 plt.ylabel("Frekuensi Dipilih")
 plt.xlabel("Waktu Keberangkatan")
 plt.xticks(ind, ('06.00', '13.00', '18.30'))
 plt.show()
 
+#Visualisasi data untuk banyak tiket terjual per posisi kursi
 df['Tempat Duduk'].value_counts().plot(kind='bar')
 plt.ylabel("Frekuensi Dipilih")
 plt.xlabel("Tempat Duduk")
